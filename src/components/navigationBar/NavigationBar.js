0eaf356c8058919ede971967857d2ab07b4e65d7
Brandon <ullman.brandon@gmail.com>
2021-09-23 16:04:20 -0700

    Added more nav-bar animation for expanding and collapsing

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavigationBar.css';
 
 export const NavigationBar = () => {
+
+        const [expand, setExpand] = useState(false);
+
+        const handleClick = () => {
+            setExpand(expand => !expand);
+        };
+
     return (
-        <div className='navigationBar'> 
-            <div className="nav-bar-name">
-                <a href='index.html'><h1 id="nav-brandon">Brandon's</h1></a>
-                <a href='index.html'><h1 id="nav-sandbox">Sandbox</h1></a>
-            </div>
-            <ul className="nav-buttons">
-                <li className="nav-button"><a href='index.html'>Home</a></li>
-                <li className="nav-button">About</li>
-                <li className="nav-button"><a href='widgets.html'>Widgets</a></li>
-                <li className="nav-button">Contact</li>
-            </ul>
-            <div className="nav-menu-button-container" onclick="menuButton(this)">
-                <div id="nav-menu-button-1" className="nav-menu-button"></div>
-                <div id="nav-menu-button-2" className="nav-menu-button"></div>
-                <div id="nav-menu-button-3" className="nav-menu-button"></div>
+        <div>
+            <div className={ !expand ? 'navigationBar' : 'navigationBar expanded' }> 
+                <div className="nav-bar-name">
+                    <a href='index.html'><h1 id="nav-brandon">Brandon's</h1></a>
+                    <a href='index.html'><h1 id="nav-sandbox">Sandbox</h1></a>
+                </div>
+                <ul className={ !expand ? 'nav-buttons' : 'nav-buttons expanded' }>
+                    <li className="nav-button"><a href='index.html'>Home</a></li>
+                    <li className="nav-button">About</li>
+                    <li className="nav-button"><a href='widgets.html'>Widgets</a></li>
+                    <li className="nav-button">Contact</li>
+                </ul>
+                <div onClick={handleClick} className="nav-expand-container">
+                    <div id="nav-expand-1" className="nav-expand"></div>
+                    <div id="nav-expand-2" className={ !expand ? 'nav-expand' : 'nav-expand expanded' }></div>
+                    <div id="nav-expand-3" className="nav-expand"></div>
+                </div>
             </div>
         </div>
     );
