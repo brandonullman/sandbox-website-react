3b24093d85d605abb0c2eeee9fb26098daac5f7d
Brandon <ullman.brandon@gmail.com>
2021-10-02 17:49:53 -0700

    Fixed CoinFlipperWidget functionality. Fixed overall styling.

--- src/components/CoinFlipperWidget/CoinFlipperWidget.js
+++ src/components/CoinFlipperWidget/CoinFlipperWidget.js
@@ -3,28 +3,32 @@ import './CoinFlipperWidget.css';
 
 export const CoinFlipperWidget = () => {
 
-    const [ coin, setCoin ] = useState('Heads');
+    const [ coin, setCoin ] = useState('');
     const [ flips, setFlips ] = useState(0);
-    
+
     const flipCoin = () => {
         let flipResult = Math.random();
         setCoin('');
-        if (flipResult <= .5) {
-            setCoin('Tails');
-        } else {
-            setCoin('Heads');
-        }
+        setTimeout(function() {
+            if (flipResult <= .5) {
+                setCoin('tails');
+            } else {
+                setCoin('heads');
+            }
+            console.log(`The RNG was ${flipResult}, you got ${coin}, you've flipped the coin ${flips} times!`);
+        }, 100);
         setFlips(prev => prev + 1);
-        console.log(`The RNG was ${flipResult}, you got ${coin}, you've flipped the coin ${flips} times!`);
     }
-  
+
     return (
         <div className='widget-container'>
             <h3>Coin Flipper</h3>
-            <div id='coin' className={coin}>
-                <div className='side-a'>Heads</div>
-                <div className='side-b'>Tails</div>
-                <div>You've flipped this coin {flips} times!</div>
+            <div>
+                <div id='coin' className={coin}>
+                    <div className='side-a'><p>Heads</p></div>
+                    <div className='side-b'><p>Tails</p></div>
+                </div>
+                <h6>You've flipped this coin {flips} times!</h6>
             </div>
             <div className='button-container'>
                 <button onClick={flipCoin} className='button'>Flip Me!</button>
